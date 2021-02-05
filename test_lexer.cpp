3cd2a6ea95a18ffd1374b5e6924df060b33d3e03
Hikmat Jafarli <jafarlihi@gmail.com>
2021-02-05 23:31:29 +0400

    Add more lexer tests

--- test_lexer.cpp
+++ test_lexer.cpp
@@ -46,5 +46,29 @@ int main(int argc, char *argv[]) {
     token = lex_one("\n \r\n \r  foo\n");
     expected_token = Token(TokenKind::NAME, 8, 11, 4, 3, nullptr, new std::string("foo"));
     assert(*token == expected_token);
+
+    token = lex_one("\n\n    foo\n\n\n");
+    expected_token = Token(TokenKind::NAME, 6, 9, 3, 5, nullptr, new std::string("foo"));
+    assert(*token == expected_token);
+
+    token = lex_one("\r\n\r\n  foo\r\n\r\n");
+    expected_token = Token(TokenKind::NAME, 6, 9, 3, 3, nullptr, new std::string("foo"));
+    assert(*token == expected_token);
+
+    token = lex_one("\r\r  foo\r\r");
+    expected_token = Token(TokenKind::NAME, 4, 7, 3, 3, nullptr, new std::string("foo"));
+    assert(*token == expected_token);
+
+    token = lex_one("\t\t  foo\t\t");
+    expected_token = Token(TokenKind::NAME, 4, 7, 1, 5, nullptr, new std::string("foo"));
+    assert(*token == expected_token);
+
+    token = lex_one("\n    #comment\n    foo#comment\n");
+    expected_token = Token(TokenKind::NAME, 18, 21, 3, 5, nullptr, new std::string("foo"));
+    assert(*token == expected_token);
+
+    token = lex_one(",,,foo,,,");
+    expected_token = Token(TokenKind::NAME, 3, 6, 1, 4, nullptr, new std::string("foo"));
+    assert(*token == expected_token);
 }
 
